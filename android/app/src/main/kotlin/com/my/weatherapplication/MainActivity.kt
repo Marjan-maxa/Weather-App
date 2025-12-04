a4f7075bb66ad9ba53d69f70f0c93a24f7d217b1
Marjan-maxa <boktimd95@gmail.com>
2025-12-04 22:39:35 +0600

    weather app

---
+++ android/app/src/main/kotlin/com/my/weatherapplication/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.my.weatherapplication
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/weather_app_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WeatherAppScreen extends StatefulWidget {
+  const WeatherAppScreen({super.key});
+
+  @override
+  State<WeatherAppScreen> createState() => _WeatherAppScreenState();
+}
+
+class _WeatherAppScreenState extends State<WeatherAppScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
