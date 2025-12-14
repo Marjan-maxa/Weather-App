3a5794f2b3ed2475526db3b344e75743a8f79801
Marjan-maxa <boktimd95@gmail.com>
2025-12-15 01:51:04 +0600

    some function added

--- lib/weather_app_screen.dart
+++ lib/weather_app_screen.dart
@@ -16,12 +16,17 @@ class _WeatherAppScreenState extends State<WeatherAppScreen> {
   bool isloading=false;
   String? _error;
   String ? _resolvedCity;
+
+  //Current
+
   double? tempC;
   int ? _wCode;
   double ? _windkmph;
   String ? _wText;
-
+String? _country;
   double ? _hi,_lo;
+
+List<_Hourly>_hourly=[];
 
 
 
@@ -48,12 +53,86 @@ class _WeatherAppScreenState extends State<WeatherAppScreen> {
 
   }
 
+  // -------------------------weather API----------------------------------
+
+
+  Future<void> fetch(String city)async {
+    setState(() {
+      isloading=true;
+      _error=null;
+    });
+
+    try{
+      final getGeo=await geoCoding(city);
+      final url=Uri.parse('https://api.open-meteo.com/v1/forecast'
+          '?latitude=${getGeo.lat}&longitude=${getGeo.lng}'
+          '&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset'
+          '&hourly=temperature_2m,weather_code,wind_speed_10m'
+          '&current=temperature_2m,weather_code,wind_speed_10m'
+          '&timezone=auto');
+      
+      final response=await http.get(url);
+      if(response.statusCode!=200)throw Exception('Forecast error${response.statusCode}');
+      final decodedData=jsonDecode(response.body) as Map<String ,dynamic>;
+
+      // Current
+
+      final current=(decodedData['current'] as Map?)??{};
+      final tempc=((current['temperature_2m']??0)as num).toDouble();
+      final wCode=(current['weather_code']as num ?)?.toInt();
+      final windkps=((current['wind_speed_10m']?? 0)as num).toDouble();
+
+
+      final hourly=(decodedData['hourly'] as Map<String,dynamic>);
+      final htime=List<String>.from(hourly['time'] as List);
+      final htemp=List<num>.from(hourly['temperature_2m'] as List);
+      final hcode=List<num>.from(hourly['weather_code'] as List);
+
+      final outHourly=<_Hourly>[];
+      for(int i=0;i<htime.length;i++){
+        outHourly.add(_Hourly(
+            time: DateTime.parse(htime[i]),
+            temp: (htemp[i]).toDouble(),
+            code: hcode[i].toInt())
+        );
+      }
+
+
+      setState(() {
+        tempC=tempc;
+        _wCode=wCode;
+        _windkmph=windkps;
+        _resolvedCity=getGeo.city;
+        _country=getGeo.country;
+        _hourly=outHourly;
+
+      });
+
+
+      print(response.body);
+    }catch(e){
+      _error=e.toString();
+    }finally{
+      setState(() {
+        isloading=false;
+      });
+    }
+  }
+
+
+
+
+
+
+
+  //  https://api.open-meteo.com/v1/forecast?latitude=23.7104&longitude=90.40744&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset,weather_code&hourly=temperature_2m,weather_code&current=temperature_2m,weather_code,wind_speed_10m&timezone=auto
+
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    geoCoding('Dhaka');
+    fetch('Dhaka');
   }
 
   @override
@@ -76,12 +155,14 @@ class _WeatherAppScreenState extends State<WeatherAppScreen> {
           child: ListView(
             padding: EdgeInsets.all(8),
             children: [
+               isloading? LinearProgressIndicator():SizedBox(),
               const SizedBox(height: 15,),
               Row(
                 children: [
                   const SizedBox(height: 10,),
                   Expanded(
                     child: TextFormField(
+                      controller: _cityController,
                       style: TextStyle(
                         color: Colors.white
                       ),
@@ -109,16 +190,16 @@ class _WeatherAppScreenState extends State<WeatherAppScreen> {
                       backgroundColor: Colors.white,
                       foregroundColor: Colors.black
                     ),
-                      onPressed: (){}, child: Text('Search'))
+                      onPressed:isloading?null:()=>fetch(_cityController.text), child: Text('Search'))
                 ],
               ),
               const SizedBox(height: 15,),
               Column(
                 children: [
-                  Text('My Locatioon',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18,color: Colors.white),),
-                  Text('Current Locatioon',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.white)),
-                  Text('37°C',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 100,color: Colors.white)),
-                  Text('Cloud',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18,color: Colors.white))
+                  Text(_country==null?"My Location":" $_country",style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20,color: Colors.white),),
+                  Text(_resolvedCity??'My Location',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18,color: Colors.white)),
+                  Text(tempC.toString(),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 100,color: Colors.white)),
+                  Text(_wCode.toString(),style: TextStyle(fontWeight: FontWeight.bold,fontSize: 18,color: Colors.white))
                 ],
               ),
               const SizedBox(height: 16,),
@@ -147,3 +228,13 @@ class _WeatherAppScreenState extends State<WeatherAppScreen> {
     );
   }
 }
+
+class _Hourly {
+  final DateTime time;
+  final double temp;
+  final int code;
+
+  _Hourly({required this.time,required this.temp,required this.code,} );
+
+}
+
